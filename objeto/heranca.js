3c64277804d3b8ccca544200d43872d8cfe783e0
Taís B Santos <tais.b.batista@ufv.br>
2021-04-20 20:24:15 -0300

    add mais sobre herança e também classe

--- objeto/heranca.js
+++ objeto/heranca.js
@@ -23,3 +23,41 @@ console.log(celta.modelo, celta.velMax)
 
 //Outra forma de definir prototipo
 Object.setPrototypeOf(celta,ferrari) //celta tem ferrari como prototipo
+
+
+
+const pai = { nome: 'Joao', corCabelo: 'castanho'}
+
+const filha = Object.create(pai)
+// filha.nome = 'Ana'
+console.log(filha.corCabelo)
+
+const filhaCacula = Object.create(pai, { 
+    nome:{ value : 'Bia', writable: false, enumerable: true }
+})
+
+console.log(Object.keys(filha)) // vai aparecer vazio pq ele não lista os valores herdados
+console.log(Object.keys(filhaCacula))
+
+for (let key in filhaCacula) {
+    filhaCacula.hasOwnProperty(key) ?
+        console.log(key) : console.log(`Por herança: ${key}`)
+
+}
+
+
+function MeuObjeto(){}
+
+console.log(MeuObjeto.prototype)
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(MeuObjeto.prototype === obj1.__proto__)
+
+MeuObjeto.prototype.nome = 'Anonimo'
+MeuObjeto.prototype.falar = function(){
+    console.log('Bom dia')
+}
+
+obj1.falar()
